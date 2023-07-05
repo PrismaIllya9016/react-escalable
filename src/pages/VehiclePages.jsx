4c39d8a2123293e49aafde647fb379d01389961d
PrismaIllyaDev <malopez091@gmail.com>
2023-07-05 05:09:37 -0700

    imagenes centradas

--- src/pages/VehiclePages.jsx
+++ src/pages/VehiclePages.jsx
@@ -8,11 +8,13 @@ function VehiclePages({ vehicle }) {
       <div id="VehiclePages" className=" m-auto text-center">
         <h1 className="  text-center mt-5 mb-5">{vehicle.name}</h1>
         <h2 className="text-center text-2xl mb-5">{vehicle.description}</h2>
-        <img
-          className=" max-w-full block"
-          src={vehicle.image}
-          alt={vehicle.name + " image"}
-        />
+        <div className="centered-image">
+          <img
+            className=" max-w-full block"
+            src={vehicle.image}
+            alt={vehicle.name + " image"}
+          />
+        </div>
       </div>
       <div className="text-center mt-10">
         <Link to={"/"}>
